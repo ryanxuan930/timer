c2443a656ab9cae0f481c4a26e1edf4e9eed49bd
Ryan Chang <anakinchung930@gmail.com>
2022-11-02 14:43:11 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/',
+  publicPath: '/timer',
   outputDir: 'docs',
   assetsDir: 'static',
 	transpileDependencies: true,

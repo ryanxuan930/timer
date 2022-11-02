326774d65763035661e2368d961cafc3e863e537
Ryan Chang <anakinchung930@gmail.com>
2022-11-02 14:09:11 +0800

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,28 @@
 <template>
-  <div class="home" style="font-size:200pt;">
-    <vue-countdown :time="60*60*1000" :interval="100" v-slot="{ hours, minutes, seconds, milliseconds }">
+  <div :class="{'yellow': color == 'yellow', 'red': color == 'red'}" style="font-size:200pt;">
+    <vue-countdown :time="110*60*1000" :interval="100" v-slot="{ hours, minutes, seconds, milliseconds }" @progress="timeData">
     {{ hours }}:{{ minutes }}:{{ seconds }}.{{ Math.floor(milliseconds / 100) }}
   </vue-countdown>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import VueCountdown from '@chenfengyuan/vue-countdown';
 
 export default defineComponent({
   name: 'HomeView',
   setup() {
+    const color = ref('black');
     return {
+      color, 
+      timeData: (data: any) => {
+        if (data.totalMinutes <= 5) {
+          color.value = 'red';
+        } else if (data.totalMinutes <= 30) {
+          color.value = 'yellow';
+        }
+      }
     }
   },
   components: {
@@ -21,3 +30,12 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="css">
+.yellow {
+  color: yellow;
+}
+.red {
+  color: red;
+}
+</style>
